911a3abb2d0efda6d0b4c86105ee86f08ad1df2f
M3mbrillo <wasinger.facundo@gmail.com>
2018-03-02 14:58:02 -0300

    Init project

---
+++ 617. Merge Two Binary Trees/617. Merge Two Binary Trees/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Reflection.Metadata.Ecma335;
+
+namespace Program
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+        }
+    }
+
+    public TreeNode MergeTrees(TreeNode t1, TreeNode t2)
+    {
+        var result = new TreeNode();
+
+
+        return result;
+    }
+
+    public class TreeNode
+    {
+        public int val;
+        public TreeNode left;
+        public TreeNode right;
+        public TreeNode(int x) { val = x; }
+    }
+
+}
