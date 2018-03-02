9ae2a673a00ae75da80d30dfbc5bd1d59a9e975c
M3mbrillo <wasinger.facundo@gmail.com>
2018-03-02 15:34:26 -0300

    Resulto 617

--- 617. Merge Two Binary Trees/617. Merge Two Binary Trees/Program.cs
+++ 617. Merge Two Binary Trees/617. Merge Two Binary Trees/Program.cs
@@ -1,23 +1,36 @@
 ﻿using System;
 using System.Reflection.Metadata.Ecma335;
 
+
 namespace Program
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            //https://leetcode.com/problems/merge-two-binary-trees/description/
+
 
         }
-    }
 
-    public TreeNode MergeTrees(TreeNode t1, TreeNode t2)
-    {
-        var result = new TreeNode();
+        public TreeNode MergeTrees(TreeNode t1, TreeNode t2)
+        {
+            
+            if (t1 == null || t2 == null)
+            {
+                return t1 ?? t2;
+            }
 
+            var result = new TreeNode(
+                    t1.val + t2.val
+                );
 
-        return result;
+            result.left = MergeTrees(t1.left, t2.left);                       
+            result.right = MergeTrees(t1.right, t2.right);
+            
+            return result;
+        }
+
     }
 
     public class TreeNode
@@ -25,7 +38,7 @@ namespace Program
         public int val;
         public TreeNode left;
         public TreeNode right;
-        public TreeNode(int x) { val = x; }
+        public TreeNode(int x) => val = x;    
     }
 
 }
